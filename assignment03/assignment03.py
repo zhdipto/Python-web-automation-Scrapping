152d243964227f260bd24374f4f9821da26f5a61
zhdipto <textzhdipto@gmail.com>
2026-06-17 21:13:14 +0600

    updated assignment03

--- assignment03/assignment03.py
+++ assignment03/assignment03.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
